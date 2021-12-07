ed0f254f02091ae8e92a37aeb7cd2d6ee8c14c89
Bruno Ramires <brunormferreira@hotmail.com>
2021-12-07 12:02:05 -0300

    implement hole app with reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: string = 'Minhas tarefas';
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: [
+        '',
+        Validators.compose([
+          Validators.minLength(3),
+          Validators.maxLength(60),
+          Validators.required,
+        ]),
+      ],
+    });
+
+    this.form.statusChanges.subscribe((status) => console.log(status));
+
+    this.todos.push(new Todo(1, 'Comprar pão', false));
+    this.todos.push(new Todo(2, 'Comprar queijo', true));
+    this.todos.push(new Todo(3, 'Comprar presunto', false));
+  }
+
+  get formValues(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  add(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    // const title = this.form.get('title')?.value;
+    const title = this.formValues['title'].value;
+    const id = this.todos.length + 1;
+
+    this.todos.push(new Todo(id, title, false));
+    this.clear();
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
+
+  clear(): void {
+    this.form.reset();
+  }
 }
---
+++ src/models/todo.model.ts
@@ -0,0 +1,7 @@
+export class Todo {
+  constructor(public id: Number, public title: String, public done: Boolean) {
+    this.id = id;
+    this.title = title;
+    this.done = done;
+  }
+}
